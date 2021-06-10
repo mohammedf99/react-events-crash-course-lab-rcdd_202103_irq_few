dba5a424986674f090806584f79854fde87ea9d1
Mohammed Mahmood <mohammed.hamidf99@gmail.com>
2021-06-10 14:16:54 +0000

    Automatically backed up by Learn

--- src/ChromeBoisDomain.js
+++ src/ChromeBoisDomain.js
@@ -44,6 +44,8 @@ export default class ChromeBoisDomain extends Component {
     return (
       <canvas 
         onMouseMove={this.handleMouseMove}
+        onClick={this.toggleHandler}
+        onKeyDown={this.resizeHandler}
         width='900'
         height='600'
         tabIndex="0">
