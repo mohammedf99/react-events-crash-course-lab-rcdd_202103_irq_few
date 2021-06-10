aac72682e8ac7b789ed8f77e420ba5a41e41398b
Mohammed Mahmood <mohammed.hamidf99@gmail.com>
2021-06-10 14:16:36 +0000

    Automatically backed up by Learn

--- src/ChromeBoisDomain.js
+++ src/ChromeBoisDomain.js
@@ -31,6 +31,14 @@ export default class ChromeBoisDomain extends Component {
   /* if the key pressed was 'a', then it should call `resize` with '+'
   /* if the key pressed was 's', then it should call `resize` with '-' 
    */
+   
+  resizeHandler = (event) => {
+  if (event.key === 'a') {
+    resize('+')
+  } else if (event.key === 's') {
+    resize('-')
+  }
+}
   
   render() {
     return (
